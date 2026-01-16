0f43535813b526b4ce31981311332c5f2a574062
Blessy Christrus D <blessychristrus21@gmail.com>
2026-01-16 11:02:48 +0530

    Refactor Transaction class with new attributes
    Updated Transaction class to include new fields and methods.

--- fraud-sim-engine/fraud-sim-engine/src/main/java/com/fraudsim/engine/model/Transaction.java
+++ fraud-sim-engine/fraud-sim-engine/src/main/java/com/fraudsim/engine/model/Transaction.java
@@ -12,19 +12,46 @@ import java.time.LocalDateTime;
 @Table(name = "transactions")
 public class Transaction {
 
-    @Id
-    private String txId;
-   private String email;
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String transactionId;
     private String accountId;
-    private double amount;
     private String merchant;
-    private String channel;
     private String location;
+    private double amount;
 
-    private int recentTxCount;
-    private double score;
-    private boolean fraud;
+    private int riskScore;
+    private String riskLevel;
+    private String status;
 
     private LocalDateTime timestamp = LocalDateTime.now();
+
+    
+    public Long getId() { return id; }
+
+    public String getTransactionId() { return transactionId; }
+    public void setTransactionId(String transactionId) { this.transactionId = transactionId; }
+
+    public String getAccountId() { return accountId; }
+    public void setAccountId(String accountId) { this.accountId = accountId; }
+
+    public String getMerchant() { return merchant; }
+    public void setMerchant(String merchant) { this.merchant = merchant; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    public double getAmount() { return amount; }
+    public void setAmount(double amount) { this.amount = amount; }
+
+    public int getRiskScore() { return riskScore; }
+    public void setRiskScore(int riskScore) { this.riskScore = riskScore; }
+
+    public String getRiskLevel() { return riskLevel; }
+    public void setRiskLevel(String riskLevel) { this.riskLevel = riskLevel; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
+
